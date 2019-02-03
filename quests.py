318057bf8e71c0669dfe6eb6f7178c6e0edcb8e9
Nikolas Martin <nikolhm@1x-193-157-236-47.uio.no>
2019-02-03 16:07:53 +0100

    Bandit Quests moved to text files

--- quests.py
+++ quests.py
@@ -270,113 +270,44 @@ def kjellprat():
 
 #Shroom: Banditter
 def banditt_q1(navn):
-    return "    Hei! Hva heter du? " + navn + """ ja, nettopp!
-    Om du vil diskutere vår angrepsstrategi mot magikerne, må du snakke med
-    Bjarte Banditt. Ellers har jeg et business-forslag som kan være svært gunstig
-    for oss begge, og du ser ut som en som kan hjelpe meg!
+    return hentQuest('shroom/banditt/banditt1/start').format(navn=navn)
 
-    For ikke lenge siden stjelte jeg fire svært verdifulle lommeur. Men det er
-    lite med ære blandt banditter, og noen har robbet dem fra meg mens jeg sov!
-    Kan du banke opp noen banditter og se om du finner de for meg? Jeg har
-    masse gullstykker jeg kan gi deg tilbake!
-
-    -- Finn fire lommeur."""
 def banditt_q1_ferdig(navn):
-    return "    MOHAHA! *ond latter*\n" +\
-    "    Tusen takk " + navn + """! Tenker de tenker seg om to ganger neste gang de lømmlene bestemmer
-    seg for å ikke invitere meg med på lommeur-jakt!\n"""
+    return hentQuest('shroom/banditt/banditt1/ferdig').format(navn=navn)
 
 def banditt_q2(navn):
-    return """    Hei du!
-    Du ser ut som en kapabel banditt som er godt kjent med sverdfekternes
-    eldgamle kampkunster. Kan du vise meg dine triks? Du skjønner det at
-    Fagre Frida, den vakreste, smarteste, mest fantastiske personen som noen
-    gang har vandret på denne jord, kun er interessert i dem som kan imponere
-    med sverdbruken sin. Dessverre er dette en av mine svakere sider! Om jeg
-    bare hadde vært like god til å svinge et sverd som jeg er til å knivstikke,
-    lyge og bedra... Akk! Men, om jeg kan henge meg på deg en stund, observere
-    dine triks og finesser, kanskje jeg da kan bli bedre?
+    return hentQuest('shroom/banditt/banditt2/start')
 
-    -- Kast Utforsk 6 ganger."""
 def banditt_q2_ferdig(navn):
-    return """\n    Neeeeeei, Fagre Frida!! Du din lømmel! Bare ikke ta sverdet mitt når jeg
-    dør! Din snik, jeg skal kv- aaaaargh *Ussle Ulv dør på dramatisk vis*\n"""
+    return hentQuest('shroom/banditt/banditt2/ferdig')
 
 def banditt_q3(navn):
-    return "    Hei " + navn + """!
-    Har du hørt om soppen? Den magiske soppen som bor her iblandt oss? De passer
-    på oss når vi går oss vill, og holder oss trygge under deres hatt. De vet
-    alt, og kjenner alle! De er i luften, i bakken, blandt åndene og blandt
-    oss levende! Dra, min disippel, til soppstedet utenfor leiren her, og hør
-    selv at de snakker direkte til deg!
+    return hentQuest('shroom/banditt/banditt3/start').format(navn=navn)
 
-    -- Dra til soppstedet og hør hva de magiske soppene har å si."""
 def banditt_q3_ferdig(navn):
-    return "    Deres vilje er vår lov! Vi må adlyde straks! Dra ut og finn disse trærne!\n"
+    return hentQuest('shroom/banditt/banditt3/ferdig')
 
 def banditt_q4(navn):
-    return "    Jaså " + navn + """, ikke helt forberedt?
-    Enhver god banditt burde være mentalt tre steg foran byttet. Avledning,
-    overraskelse og illusjoner, dette er mine spesialiteter! Vil du lære å
-    finpusse din egen smidighet, forvirre fiendens sinn og stikke av med de
-    saftigste byttene? Jeg kan vise deg hvordan man senker konsentrasjonen
-    til fienden, se og lær!
+    return hentQuest('shroom/banditt/banditt4/start').format(navn=navn)
 
-    Skriv 'di' eller 'distraher' for å kaste Distraher på fienden!
-
-    -- Distraher fiender 9 ganger."""
 def banditt_q4_ferdig(navn):
-    return "    Utmerket " + navn + """!
-    med slike ferdigheter vil du nok raskt øke din fryktinngytenhet!\n"""
+    return hentQuest('shroom/banditt/banditt4/ferdig').format(navn=navn)
 
 def banditt_q5(navn):
-    return "    " + navn + """!
-    For et strålende talent! Slik teknikk! Og så tiltrekkende hender!
-    Jeg kunne ikke unngå å legge merke til hvor effektivt du slo ned den
-    knivstikkende slasken Ussle Ulf. Si meg, er du interessert i å gjøre
-    meg en tjeneste? Det har seg nemlig slik at eksen min, Onde Olga, har
-    blitt særdeles hovmoden i det siste. Hun pleide å være tolererbar, men
-    i det siste har hun blitt umotståel- ehm, utålelig! Hun burde virkelig
-    bli jekket ned et par hakk, til felles gode for alle her i leiren. Hun
-    pleier å henge ved duellringen, kanskje du kan offentlig bekjemper henne
-    med dine fantastiske evner i ringen? Så kan vi se nærmere på dine evner
-    privat etterpå.
+    return hentQuest('shroom/banditt/banditt5/start').format(navn=navn)
 
-    -- Bekjemp Onde Olga i duellringen."""
 def banditt_q5_ferdig(navn):
-    return "    For en lykkedag! Tusen takk " + navn + """!
-    Onde Olga har startet å vise interesse for meg igjen! Jeg bare vet
-    at vi kommer til å utrette virkelig grufulle ting sammen! Ekte
-    kjærlighet er absolutt en sjelden vare, husk på det """ + navn + """.
-    Vi to fikk aldri en sjanse sammen, men jeg er sikker på at den rette for
-    deg er der ute et sted.\n"""
+    return hentQuest('shroom/banditt/banditt5/ferdig').format(navn=navn)
 
 def banditt_q6(navn):
-    return "    Hvem er du? Du ser ut som en " + navn + ". Yup, definitivt " + navn + """.
-    Uansett hvem du er, her er vi alle felles banditter, og i disse harde tidene må
-    vi banditter stå sammen mot de ondskapsfulle magikerne! De har alltid vært en
-    pest og en plage, stanset våre robberier og jaget oss vekk til denne skogen.
-    Men de har aldri direkte angrepet oss før, ikke slik som nå. Vi er ikke helt
-    sikre på hva de har gjort, men denne ellers medgjørlige skogen har i det siste
-    blitt... levende. På et vis. I tillegg har våre medbanditter oppført seg særdeles
-    merkverdig i det siste. Jeg er sikker på at dette er Kjedelige Kjell sitt verk!
-    Han er allerede notorisk for sin evne til å kjede folk til døde, men han er og
-    gal etter alt som har med trær og natur å gjøre. Om bare han hadde vært
-    'ordnet', kunne vi gått tilbake til vår vanlige tilværelse. Kan du ordne saken?
-    Kutt av ham fingeren til bevis!
+    return hentQuest('shroom/banditt/banditt6/start').format(navn=navn)
 
-    -- Kutt av fingeren til Kjedelige Kjell og vis den til Bjarte Banditt."""
 def banditt_q6_ferdig(navn):
-    return "    Er dette den ekte fingeren til Kjedelige Kjell? Fantastisk " + navn + """!
-    Dette må jo bety at deres utspekulerte planer om å gjøre skogen levende har
-    kommet til en stopp. Utmerket! Angrep fra skogen vil opphøre, folk vil
-    returnere til normalen, de hersens sopp-fanatikerne vil åpne øynene, kanskje
-    til og med det store treet over oss som plutselig dukket opp vil trekke
-    seg tilbak- """ + Fore.RED + "Hei " + navn + """. Vi har ventet på deg. Disse fjolsene er snart under
-    vår kontroll, de vil ikke forstyrre dere lengre. Dette neket tar vi med
-    oss. Vi vil sees igjen """ + navn + ". " + Style.RESET_ALL + """Huff da, jeg ble litt svimmel. Uansett, alt
-    vil bli bra fremover, tusen takk! Jeg tror jeg skal ta meg en tur ut i
-    skogen for å feire, jeg fikk plutselig veldig lyst på sopp...\n"""
+    return hentQuest('shroom/banditt/banditt6/ferdig').format(
+         navn=navn,
+         red=Fore.RED,
+         reset=Style.RESET_ALL
+     )
 
 def banditt_dq7(navn):
     return "    Hei " + navn + """!
